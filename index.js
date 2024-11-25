dc224bd127c10537d7ade2bcea0e3480d632f239
Mayank Panchal <mayank@screetract.com>
2024-11-25 12:26:34 +0530

    first project setted up

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/about", (req, res) => {
+    res.send("This is about page!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
