73c8611c41e3149a811dfd312fd10bfd8fc7aca5
Mayank Panchal <mayank@screetract.com>
2024-11-25 14:50:26 +0530

    Change update

--- index.js
+++ index.js
@@ -2,15 +2,63 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    const arr = [
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Jane Smith" },
+        { id: 3, name: "Bob Johnson" },
+    ];
+
+    res.json(arr);
+});
+const books = [
+    { id: 1, title: "The Lord of the Rings" },
+    { id: 2, title: "Harry Potter and the Sorcerer's Stone" },
+    { id: 3, title: "Pride and Prejudice" },
+    { id: 4, title: "To Kill a Mockingbird" },
+    { id: 5, title: "1984" },
+    { id: 6, title: "The Great Gatsby" },
+    { id: 7, title: "The Catcher in the Rye" },
+    { id: 8, title: "The Adventures of Huckleberry Finn" },
+    { id: 9, title: "To Kill a Mockingbird" },
+    { id: 10, title: "1984" },
+    { id: 11, title: "The Great Gatsby" },
+    { id: 12, title: "The Catcher in the Rye" },
+    { id: 13, title: "The Adventures of Huckleberry Finn" },
+];
+
+// Get all books
+app.get("/books", (req, res) => {
+    res.json(books);
 });
 
-app.get("/about", (req, res) => {
-    res.send("This is about page!");
+// Get a specific book
+app.get("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const book = books.find((book) => book.id === id);
+    if (book) {
+        res.json(book);
+    } else {
+        res.status(404).json({ message: "Book not found" });
+    }
 });
-app.get("/contact", (req, res) => {
-    res.send("This is contact page!");
+
+// Update a book
+app.put("/books/:id", (req, res) => {
+    console.log(req.body);
+    const id = parseInt(req.params.id);
+    const bookIndex = books.findIndex((book) => book.id === id);
+
+    if (bookIndex !== -1) {
+        books[bookIndex] = req.body;
+
+        res.json(books[bookIndex]);
+    } else {
+        res.status(404).json({ message: "Book not found" });
+    }
 });
 
 const port = process.env.PORT || 3000;
